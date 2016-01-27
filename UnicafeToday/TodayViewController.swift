56a56546a25dad14a4c89c0236d900efbcffe1c1
Sami Saada <sami.saada@gmail.com>
2016-01-27 16:56:41 +0200

    Add Today

---
+++ UnicafeToday/TodayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TodayViewController.swift
+//  UnicafeToday
+//
+//  Created by Sami Saada on 27.01.2016.
+//
+//
+
+import Cocoa
+import NotificationCenter
+
+class TodayViewController: NSViewController, NCWidgetProviding {
+
+    override var nibName: String? {
+        return "TodayViewController"
+    }
+
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Update your data and prepare for a snapshot. Call completion handler when you are done
+        // with NoData if nothing has changed or NewData if there is new data since the last
+        // time we called you
+        completionHandler(.NoData)
+    }
+
+}
