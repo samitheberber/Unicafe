c3a6c56070bf5ffd51d00f1f7b6b21343243f376
Sami Saada <sami.saada@gmail.com>
2016-01-29 17:37:15 +0200

    basic structure done

--- UnicafeToday/TodayViewController.swift
+++ UnicafeToday/TodayViewController.swift
@@ -11,28 +11,46 @@ import NotificationCenter
 
 class TodayViewController: NSViewController, NCWidgetProviding {
 
+    var lunchesForToday: [Lunch]?
+
     override var nibName: String? {
         return "TodayViewController"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
         // Update your data and prepare for a snapshot. Call completion handler when you are done
         // with NoData if nothing has changed or NewData if there is new data since the last
         // time we called you
-        lol()
-        completionHandler(.NoData)
+        fetchData() {
+            (result: Bool, lunches: [Lunch]) in
+            if result {
+                self.lunchesForToday = lunches
+                self.updateLunches()
+                completionHandler(.NewData)
+            } else {
+                completionHandler(.NoData)
+            }
+        }
     }
 
-    func lol() {
+    func updateLunches() {
+        for lunch in self.lunchesForToday! {
+            print(lunch.name)
+        }
+    }
+
+    func fetchData(completion: (result: Bool, lunches: [Lunch]) -> Void) {
         LunchFetcher.getLunches({(week: [DailyLunches]) -> Void in
             let lunches = self.getLunchesFromToday(week)
-            for lunch in lunches {
-                print(lunch.name)
-            }
+            completion(result: true, lunches: lunches!)
         })
     }
 
-    func getLunchesFromToday(week: [DailyLunches]) -> [Lunch] {
+    func getLunchesFromToday(week: [DailyLunches]) -> [Lunch]? {
         let currentDate = NSDate()
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "EEE dd.MM"
@@ -43,7 +61,7 @@ class TodayViewController: NSViewController, NCWidgetProviding {
             }
             //print(day.lunches?.first?.price)
         }
-        return []
+        return nil
     }
 
 }
